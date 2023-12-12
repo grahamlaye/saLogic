4b7be9ef044730aed735be76838d952e9089c405
grahamlaye <107111464+grahamlaye@users.noreply.github.com>
2023-12-12 09:33:26 +0000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,15 +1,27 @@
 #!/bin/bash
-# Get Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
-node -e "console.log('Running Node.js ' + process.version)"
 
-# Install npm
-sudo dnf install npm -y
+# Check if Node.js is installed
+if command -v node &> /dev/null; then
+    echo "Node.js is already installed."
+else
+    # Get Node.js
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+    . ~/.nvm/nvm.sh
+    nvm install --lts
+    node -e "console.log('Running Node.js ' + process.version)"
+fi
+
+# Check if npm is installed
+if command -v npm &> /dev/null; then
+    echo "npm is already installed."
+else
+    # Install npm
+    sudo dnf install npm -y
+fi
 
 # Install required packages
 npm install package.json
 
 # Check installed packages
 npm ls
+

319ee77efa41a3aefa1efe83f0faf3229464af56
grahamlaye <107111464+grahamlaye@users.noreply.github.com>
2023-12-19 18:24:55 +0000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -29,16 +29,33 @@ fi
 # Check installed packages
 npm ls
 
-# Stop OS from recording history for this session.
+# Stop recording history for this session
 set +o history
 
-# Set environment variables for this session only.
-read -p "Enter API Key: " apiKey
-export apiKey
+# Secrets method selector
+read -p "Please select a method for the webSocket.js file to retrieve apiKey, apiSecret and workspaceID values.
 
-read -p "Enter API Secret: " apiSecret
-export apiSecret
+AWS Secrets Manager (aws)
+Environment Variables (env)
+type aws or env:" secretChoice
 
-read -p "Enter Workspace ID: " workspaceID
-export workspaceID
+if [[ $secretChoice == "aws" ]]; then
+    # Set AWS environment variables for this session only
+    read -p "Enter AWS Region name (example: us-east-1): " awsRegion
+    export awsRegion
 
+    read -p "Enter AWS Secrets Name (example: api-demo-secrets): " secretsName
+    export secretsName
+
+elif [[ $secretChoice == "env" ]]; then
+
+    # Set environment variables for this session only
+    read -p "Enter API Key: " apiKey
+    export apiKey
+
+    read -p "Enter API Secret: " apiSecret
+    export apiSecret
+
+    read -p "Enter Workspace ID: " workspaceID
+    export workspaceID
+fi
